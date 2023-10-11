861f99225cf01af614411dc0c2c1e743d4e4259b
Lauraczuluagac <lauris.0208@gmail.com>
2023-10-11 18:33:31 -0500

    se agrega aplicatvio

--- pages/3_Aplicación 2.py
+++ pages/3_Aplicación 2.py
@@ -1,21 +1,46 @@
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
+import tkinter as tk
 
-# Crea un conjunto de datos de ejemplo
-import pandas as pd
+def calcular():
+    try:
+        resultado.set(eval(entrada.get()))
+    except:
+        resultado.set("Error")
 
-data = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+# Crear una ventana principal
+ventana = tk.Tk()
+ventana.title("Calculadora")
 
-# Crea un diagrama de dispersión
-sns.scatterplot(x='sepal_length', y='sepal_width', data=data, hue='species')
+# Variable para mostrar el resultado
+resultado = tk.StringVar()
 
-# Agrega etiquetas y título
-plt.xlabel('Longitud del Sépalo')
-plt.ylabel('Ancho del Sépalo')
-plt.title('Diagrama de Dispersión de Longitud del Sépalo vs. Ancho del Sépalo')
+# Crear una entrada de texto
+entrada = tk.Entry(ventana, width=30)
+entrada.grid(row=0, column=0, columnspan=4)
 
-# Muestra el gráfico
-plt.show()
+# Crear botones para números y operaciones
+botones = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+'
+]
 
+row_val = 1
+col_val = 0
+
+for button in botones:
+    tk.Button(ventana, text=button, padx=20, pady=20, command=lambda b=button: entrada.insert('end', b) if b != '=' else calcular()).grid(row=row_val, column=col_val)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
+
+# Botón Clear
+tk.Button(ventana, text='Clear', padx=20, pady=20, command=lambda: entrada.delete(0, 'end')).grid(row=row_val, column=col_val)
+
+# Mostrar el resultado
+tk.Label(ventana, textvariable=resultado, padx=20, pady=20).grid(row=row_val + 1, columnspan=4)
+
+ventana.mainloop()
 st.header("Aplicacón 2")
