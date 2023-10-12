8906d2c3425d0babb6430ae0c668bb8f14c904c7
Lauraczuluagac <lauris.0208@gmail.com>
2023-10-11 19:08:43 -0500

    co

--- pages/3_Aplicación 2.py
+++ pages/3_Aplicación 2.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import tkinter as tk
 
+
 def calcular():
     try:
         resultado.set(eval(entrada.get()))
     except:
         resultado.set("Error")
+
 
 # Crear una ventana principal
 ventana = tk.Tk()
@@ -30,17 +32,20 @@ row_val = 1
 col_val = 0
 
 for button in botones:
-    tk.Button(ventana, text=button, padx=20, pady=20, command=lambda b=button: entrada.insert('end', b) if b != '=' else calcular()).grid(row=row_val, column=col_val)
+    tk.Button(ventana, text=button, padx=20, pady=20, command=lambda b=button: entrada.insert(
+        'end', b) if b != '=' else calcular()).grid(row=row_val, column=col_val)
     col_val += 1
-    if col_val > 3:
-        col_val = 0
-        row_val += 1
+if col_val > 3:
+    col_val = 0
+    row_val += 1
 
 # Botón Clear
-tk.Button(ventana, text='Clear', padx=20, pady=20, command=lambda: entrada.delete(0, 'end')).grid(row=row_val, column=col_val)
+tk.Button(ventana, text='Clear', padx=20, pady=20, command=lambda: entrada.delete(
+    0, 'end')).grid(row=row_val, column=col_val)
 
 # Mostrar el resultado
-tk.Label(ventana, textvariable=resultado, padx=20, pady=20).grid(row=row_val + 1, columnspan=4)
+tk.Label(ventana, textvariable=resultado, padx=20,
+         pady=20).grid(row=row_val + 1, columnspan=4)
 
 ventana.mainloop()
 st.header("Aplicacón 2")
