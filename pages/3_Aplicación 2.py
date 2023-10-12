ed26c464ae52e08bac6cd3bb0d8cfeefec8b13f6
Lauraczuluagac <lauris.0208@gmail.com>
2023-10-11 19:06:35 -0500

    com

--- pages/3_Aplicación 2.py
+++ pages/3_Aplicación 2.py
@@ -1,16 +1,46 @@
 import streamlit as st
-import cv2
+import tkinter as tk
 
-# Cargar una imagen
-imagen = cv2.imread('dogcat.avif')
+def calcular():
+    try:
+        resultado.set(eval(entrada.get()))
+    except:
+        resultado.set("Error")
 
-# Aplicar un filtro de desenfoque
-imagen_desenfocada = cv2.GaussianBlur(imagen, (15, 15), 0)
+# Crear una ventana principal
+ventana = tk.Tk()
+ventana.title("Calculadora")
 
-# Mostrar la imagen original y la imagen desenfocada
-cv2.imshow('Imagen Original', imagen)
-cv2.imshow('Imagen Desenfocada', imagen_desenfocada)
-cv2.waitKey(0)
+# Variable para mostrar el resultado
+resultado = tk.StringVar()
 
-cv2.destroyAllWindows()
+# Crear una entrada de texto
+entrada = tk.Entry(ventana, width=30)
+entrada.grid(row=0, column=0, columnspan=4)
+
+# Crear botones para números y operaciones
+botones = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+'
+]
+
+row_val = 1
+col_val = 0
+
+for button in botones:
+    tk.Button(ventana, text=button, padx=20, pady=20, command=lambda b=button: entrada.insert('end', b) if b != '=' else calcular()).grid(row=row_val, column=col_val)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
+
+# Botón Clear
+tk.Button(ventana, text='Clear', padx=20, pady=20, command=lambda: entrada.delete(0, 'end')).grid(row=row_val, column=col_val)
+
+# Mostrar el resultado
+tk.Label(ventana, textvariable=resultado, padx=20, pady=20).grid(row=row_val + 1, columnspan=4)
+
+ventana.mainloop()
 st.header("Aplicacón 2")
